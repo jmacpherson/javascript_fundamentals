41649f641f71620a5ff7b46f424ef825c6a45b26
jmacpherson <jd.macpherson@gmail.com>
2013-11-20 15:30:07 -0500

    completing variables exercise

--- js_fundamentals/variables.js
+++ js_fundamentals/variables.js
@@ -7,6 +7,16 @@
 // Store the following into variables: number of children, partner's name, geographic location, job title.
 // Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 
+var locale = "the Universe and beyond";
+
+var no_of_children = 17;
+
+var partner = "Nuit";
+
+var job_title = "Magician";
+
+alert("You will be a " + job_title + " in " + locale + ", and you will be married to " + partner + " with " + no_of_children + " children.");
+
 // EXERCISE: The Age Calculator
 
 // Forgot how old someone is? Calculate it!
@@ -15,6 +25,18 @@
 // Store their birth year in a variable.
 // Calculate their 2 possible ages based on the stored values.
 // Output them to the screen like so: "They are either NN or NN", substituting the values.
+
+var year = new Date();
+
+year = year.getFullYear();
+
+var birth_year = 1984;
+
+var ageIfBirthdayPassed = year - birth_year
+
+var ageIfBirthdayNotPassed = ageIfBirthdayPassed - 1;
+
+alert("They are either " + ageIfBirthdayPassed + " or " + ageIfBirthdayNotPassed + ".");
 
 // EXERCISE: The Lifetime Supply Calculator
 
@@ -26,6 +48,17 @@
 // Calculate how many you would eat total for the rest of your life.
 // Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
 
+var currentAge = 27;
+undefined
+var maximumAge = 7000000;
+undefined
+var estimatedServingsOfPadThaiPerDay = 1;
+undefined
+var lifeTimeSupplyOfPadThai = (maximumAge - currentAge) * estimatedServingsOfPadThaiPerDay;
+undefined
+alert("You will need " + lifeTimeSupplyOfPadThai + " servings of Pad Thai to last you until the ripe old age of " + maximumAge + ".");
+undefined
+
 // EXERCISE: The Geometrizer
 
 // Calculate properties of a circle, using the definitions here.
@@ -33,6 +66,17 @@
 // Store a radius into a variable.
 // Calculate the circumference based on the radius, and output "The circumference is NN".
 // Calculate the area based on the radius, and output "The area is NN".
+
+var radius = 17;
+undefined
+var circumference = 2 * Math.PI * radius;
+undefined
+circumference
+106.81415022205297
+var area = (Math.PI * radius) * (Math.PI * radius)
+undefined
+area
+2852.3156719148246
 
 // EXERCISE: The Temperature Converter
 
@@ -43,4 +87,21 @@
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
 
+var tempInC = 23;
+undefined
+var tempInF = ((tempInC * 9) / 5) + 32
+undefined
+tempInF
+73.4
+alert(tempInC + "˚C is " + tempInF + "˚F.");
+undefined
 
+
+var tempInF = 89;
+undefined
+var tempInC = ((tempInF - 32) * 5) / 9;
+undefined
+tempInC
+31.666666666666668
+alert(tempInF + "˚F is " + tempInC + "˚C.");
+undefined
