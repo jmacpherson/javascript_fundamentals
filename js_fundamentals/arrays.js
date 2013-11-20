06df317279413ca847db2c8337971387f61e3831
jmacpherson <jd.macpherson@gmail.com>
2013-11-20 17:53:06 -0500

    completing array exercise.

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -6,5 +6,13 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
-
-
+var topChoices = ["Purple",73,"Kitten","Lime","oxygen",89];
+undefined
+for(var i = 0;i < topChoices.length; i++) {console.log("My #"+ (i+1) + " choice is " + topChoices[i] + ".");};
+My #1 choice is Purple. VM8315:2
+My #2 choice is 73. VM8315:2
+My #3 choice is Kitten. VM8315:2
+My #4 choice is Lime. VM8315:2
+My #5 choice is oxygen. VM8315:2
+My #6 choice is 89. VM8315:2
+undefined
