1b93bbcafd44f0e34a1449136729e542357eb000
jmacpherson <jd.macpherson@gmail.com>
2013-11-21 15:53:25 -0500

    completing objects exercises

---
+++ js_fundamentals/example.js
@@ -0,0 +1,3 @@
+for (var i = 0; i <= 10; i++) {
+  document.write("We're writing to the page " + i + " times!<br />");
+}
--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -13,6 +13,20 @@
 // cumin
 // cocoa
 
+function Recipe(name, servings, ingredients) {
+  this.name = name;
+  this.servings = servings;
+  this.ingredients = ingredients;
+  this.display = function (){
+    console.log(this.name);
+    console.log(this.servings);
+    console.log(this.ingredients);
+  };
+};
+
+greenSmoothie = new Recipe("Green Smoothie", "1", ["kale", "banana", "spirulina", "hemp", "maca", "juice", "almond milk"])
+greenSmoothie.display();
+
 // EXERCISE: The Reading List
 
 // Keep track of which books you read and which books you want to read!
@@ -21,6 +35,34 @@
 // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
 
+function Book(title, author, alreadyRead) {
+  this.title = title;
+  this.author = author;
+  this.alreadyRead = alreadyRead;
+  this.display = function() {
+    if (alreadyRead) {
+      console.log("You have already read " + this.title + " by " + this.author + ".");
+    } else {
+      console.log("You have not yet read " + this.title + " by " + this.author + ".");
+    };
+  };
+};
+
+var library = {
+  books: [],
+  addBook: function(book) {
+    this.books.push(book);
+  },
+  display: function() {
+    for (var i = 0; i < this.books.length; i++) {
+      this.books[i].display();
+    }
+  }
+};
+
+library.addBook(new Book("Snow Crash", "Neal Stephenson", false));
+library.display();
+
 // EXERCISE: The Movie Database
 
 // It's like IMDB, but much much smaller!
@@ -28,5 +70,14 @@
 // Create an object to store the following information about your favorite movie: title (a string), duration (a number), and stars (an array of strings).
 // Create a function to print out the movie information like so: "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
 
+var favouriteMovie = {
+  title: "The Neverending Story",
+  duration: 95,
+  stars: ["Atreyu", "Artax", "Falcor"],
+  display: function(){
+    console.log(this.title + " lasts for " + this.duration + ".");
+    console.log("Stars: " + this.stars.join(", ") + ".");
+  }
+};
 
-
+favouriteMovie.display();
